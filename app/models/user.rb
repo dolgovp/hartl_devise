94482f1d2ccbd4f840fcc9a96a8ace40ca4b22a5
Павел Долгов <dolgoff1272@gmail.com>
2021-11-19 13:37:26 +0300

    chapter 8 fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    attr_accessor :remeber_token
+    attr_accessor :remember_token
     before_save {self.email = email.downcase}
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :name, presence: true, length: { maximum: 50}
